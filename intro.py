321747a2c7dfd72ac70bb40ea67465a8e1262ab1
Narmadha <knarmadhaeie@gmail.com>
2022-05-16 17:45:59 +0530

    added all files

---
+++ intro.py
@@ -0,0 +1,47 @@
+"""
+Data types:
+1. Number
+2. Strings
+3. Lists
+4. Tuple
+5. Sets
+6. Dictionaries
+"""
+# Number
+a = 1
+print(type(a))
+
+b = 1.2
+print(type(b))
+
+# Strings
+place = "Bangalore"
+print(type(place))
+
+# Lists
+list1 = [1, "a", 2, 3, 'hyderabad', 2.2, [1, 2]]
+print(type(list1))
+
+# # Tuple:
+tuple1 = ("a", [1, 2], 23)
+print(type(tuple1))
+
+# Set
+set1 = {1, 2, (1, 2, 3), "python"}
+print(set1)
+print(type(set1))
+
+# Dictionaries
+dict1 = {"a": 1, "b": 2}
+print(type(dict1))
+
+str1 = ''
+list2 = list()
+list3 = []
+tuple2 = tuple()
+tuple3 = ()
+set2 = set()
+set3 = {}
+dict2 = dict()
+dict3 = {}
+print(type(set3))
